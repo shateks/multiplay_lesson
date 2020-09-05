945f28bae4ca614c67f5e1b9c8aa1d4e45f13e22
marcin <szateks@gmail.com>
2020-09-05 18:09:35 +0200

    Change way of funtioncing of "genMultiplayCombinations". And ofcourse in test arrays.

--- src/constants/combinations.js
+++ src/constants/combinations.js
@@ -1,31 +1,39 @@
 class MulOperation {
   constructor(first, second) {
-    if (second > first) {
-      this.first = first
-      this.second = second
-    } else {
-      this.first = second
-      this.second = first
-    }
-    this.id = first.toString() + "x" + second.toString()
+    this.first = first
+    this.second = second
+    if (first > second) [this.first, this.second] = [this.second, this.first]
+    this.id = this.first.toString() + "x" + this.second.toString()
     this.result = first * second
   }
 }
 
-class MulCollection {}
+// class MulCollection {}
+
 const genMultiplayCombinations = function (firstNumber, secondNumber) {
-  let a, b
   const ret = {}
-  if (firstNumber > secondNumber) {
-    a = secondNumber
-    b = firstNumber
-  } else {
-    a = firstNumber
-    b = secondNumber
-  }
-  for (let i = a; i <= b; i++) {
-    for (let j = i; j <= b; j++) {
-      //   basicCell(i, j)
+  // let a, b
+  // if (firstNumber > secondNumber) {
+  //   a = secondNumber
+  //   b = firstNumber
+  // } else {
+  //   a = firstNumber
+  //   b = secondNumber
+  // }
+  // for (let i = a; i <= b; i++) {
+  //   for (let j = i; j <= b; j++) {
+  //     let m = new MulOperation(i, j)
+  //     ret[m.id] = m
+  //   }
+  // }
+  // debugger
+  firstNumber = firstNumber ?? secondNumber
+  secondNumber = secondNumber ?? firstNumber
+  if (firstNumber > secondNumber)
+    [firstNumber, secondNumber] = [secondNumber, firstNumber]
+
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    for (let j = 1; j <= 9; j++) {
       let m = new MulOperation(i, j)
       ret[m.id] = m
     }
--- src/constants/combinations.test.js
+++ src/constants/combinations.test.js
@@ -4,12 +4,61 @@ test("Basic if true is true", () => {
   expect(true).toBe(true)
 })
 
-const test_array = ["7x7", "7x8", "7x9", "8x8", "8x9", "9x9"]
+const test_array_5 = [
+  "1x5",
+  "2x5",
+  "3x5",
+  "4x5",
+  "5x5",
+  "5x6",
+  "5x7",
+  "5x8",
+  "5x9",
+]
+const test_array_7to9 = [
+  "1x7",
+  "2x7",
+  "3x7",
+  "4x7",
+  "5x7",
+  "6x7",
+  "7x7",
+  "7x8",
+  "7x9",
+  "1x8",
+  "2x8",
+  "3x8",
+  "4x8",
+  "5x8",
+  "6x8",
+  "8x8",
+  "8x9",
+  "1x9",
+  "2x9",
+  "3x9",
+  "4x9",
+  "5x9",
+  "6x9",
+  "9x9",
+]
 
 test("Multable 7 to 9", () => {
-  debugger
   const mulObj = MulTo9.genMultiplayCombinations(7, 9)
-  for (let element of test_array) {
+  for (let element of test_array_7to9) {
+    expect(mulObj).toHaveProperty(element)
+  }
+})
+
+test("Multable 9 to 7", () => {
+  const mulObj = MulTo9.genMultiplayCombinations(9, 7)
+  for (let element of test_array_7to9) {
+    expect(mulObj).toHaveProperty(element)
+  }
+})
+
+test("Multable only 5", () => {
+  const mulObj = MulTo9.genMultiplayCombinations(5)
+  for (let element of test_array_5) {
     expect(mulObj).toHaveProperty(element)
   }
 })
