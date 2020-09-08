4b970471eca40f7e5659948e3415226866f81a1b
marcin <szateks@gmail.com>
2020-09-08 22:35:04 +0200

    Added icons from react-icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,17 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { FaHome } from "react-icons/fa"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="header">
+    <div id="header-title">
+      <h1 style={{ margin: 0 }}>{siteTitle}</h1>
+    </div>
+    <div id="home-button">
+      <Link className="header-link" to="/">
+        <FaHome id="home-icon" />
+      </Link>
     </div>
   </header>
 )
