c2750cbd84653a2cd2e70d9afd1992769c9ee102
marcin <szateks@gmail.com>
2020-11-06 11:07:19 +0100

    Ugly way to pass gatsby build, error while trying to invoke localstorage.

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,14 +1,24 @@
 import conf from "../constants/configconsts"
+
+const isNode = () => typeof window === "undefined"
+
+const getItem = str => {
+  if (isNode()) {
+    return false
+  }
+  return localStorage.getItem(str)
+}
+
 export const getRange = () => {
-  const nines = localStorage.getItem(conf.nines.str) == "true"
-  const eights = localStorage.getItem(conf.eights.str) == "true"
-  const sevens = localStorage.getItem(conf.sevens.str) == "true"
-  const sixes = localStorage.getItem(conf.sixes.str) == "true"
-  const fives = localStorage.getItem(conf.fives.str) == "true"
-  const fours = localStorage.getItem(conf.fours.str) == "true"
-  const threes = localStorage.getItem(conf.threes.str) == "true"
-  const twos = localStorage.getItem(conf.twos.str) == "true"
-  const ones = localStorage.getItem(conf.ones.str) == "true"
+  const nines = getItem(conf.nines.str) == "true"
+  const eights = getItem(conf.eights.str) == "true"
+  const sevens = getItem(conf.sevens.str) == "true"
+  const sixes = getItem(conf.sixes.str) == "true"
+  const fives = getItem(conf.fives.str) == "true"
+  const fours = getItem(conf.fours.str) == "true"
+  const threes = getItem(conf.threes.str) == "true"
+  const twos = getItem(conf.twos.str) == "true"
+  const ones = getItem(conf.ones.str) == "true"
 
   const range = [
     nines && 9,
@@ -24,11 +34,13 @@ export const getRange = () => {
   return range
 }
 
-export const toggleNumer = name => {
+const toggleNumer = name => {
   let val = localStorage.getItem(name.str)
   localStorage.setItem(name.str, (!(val == "true")).toString())
 }
 
-export const getNumer = name => {
-  return localStorage.getItem(name.str) == "true"
+const getNumer = name => {
+  return getItem(name.str) == "true"
 }
+
+export { toggleNumer, getNumer }
