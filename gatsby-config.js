5cac02f13396e1db90b7135763095434c7060299
marcin <szateks@gmail.com>
2020-11-06 18:02:21 +0100

    Removing gatsby starter remains

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Multiplay Lesson`,
+    description: `Small lesson of multiplay.`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-intl`,
       options: {
-        // language JSON resource path
         path: `${__dirname}/src/intl`,
-        // supported language
         languages: [`en`, `pl`],
-        // language file path
         defaultLanguage: `en`,
-        // option to redirect to `/ko` when connecting `/`
         redirect: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
